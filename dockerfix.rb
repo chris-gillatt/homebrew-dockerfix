83404c16c5b076aa27254d214bf5b7276df0f49b
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2018-10-09 15:20:35 +0100

    Push new version of dockerfix

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
   desc "An app to help clean up your docker environment"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.3.tar.gz"
-  sha256 "ef67de4f5db26fecfea31cd5624efed3877844e7a2f1fc7b7e3060360f031a98"
-  version "0.0.3"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.4.tar.gz"
+  sha256 "99772b6d1f6e14b97c80e2dd332cf970bcb379bc65807609ef17e54f4e645735"
+  version "0.0.4"
 
   depends_on "curl"
 
