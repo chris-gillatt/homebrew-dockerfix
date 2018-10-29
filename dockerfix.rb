3d51a1661baafc65d2a85ff5dcbc96619ed23905
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2018-10-29 11:33:55 +0000

    Bug fixes * Fix stop running containers from getting a 404 from the docker daemon * Allow ability to combine flags like dockerfix

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
   desc "An app to help clean up your docker environment"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.5.tar.gz"
-  sha256 "e31d7c24594df0d74b756f666973b9ce175d947b4734a8051223a129015b5210"
-  version "0.0.5"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.6.tar.gz"
+  sha256 "7e79dcd004d3a26173857fe0f11871f8cdfded58508cc5eeca428d606cea3b2e"
+  version "0.0.6"
 
   depends_on "curl"
 
