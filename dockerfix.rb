53b53e002d2a2d84fa95597b3ea54bbdb733da2b
Chris Gillatt <chris.gillatt@autotrader.co.uk>
2018-11-08 13:02:11 +0000

    Correct function call for stop all running containers for -a

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
   desc "An app to help clean up your docker environment"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.6.tar.gz"
-  sha256 "7e79dcd004d3a26173857fe0f11871f8cdfded58508cc5eeca428d606cea3b2e"
-  version "0.0.6"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.7.tar.gz"
+  sha256 "0e456bc6a5253afc9734a0141257c0ddd63d2eb173328a5f9a86610d7f761431"
+  version "0.0.7"
 
   depends_on "curl"
 
