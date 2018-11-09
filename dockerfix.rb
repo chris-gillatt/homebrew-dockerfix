19d406f10a5363a838488830dda504b6c98a56e6
chris.gillatt <chris.gillatt@autotrader.co.uk>
2018-11-09 15:19:05 +0000

    Minor changes * Remove -l login to gcp from help messages as this will be deprecated * Tidy some code * Add docker update check to -d

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
   desc "An app to help clean up your docker environment"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.7.tar.gz"
-  sha256 "0e456bc6a5253afc9734a0141257c0ddd63d2eb173328a5f9a86610d7f761431"
-  version "0.0.7"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.8.tar.gz"
+  sha256 "16ac11cb58b3157176cb0730352b6197a579fd497abbc5f75b8f6558f3fb5ecc"
+  version "0.0.8"
 
   depends_on "curl"
 
