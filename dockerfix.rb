21972b954b74130d2dc001533284269461690a7c
chris.gillatt <chris.gillatt@autotrader.co.uk>
2018-11-14 17:02:40 +0000

    Improve readme, help and some of the outputs

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
-  desc "An app to help clean up your docker environment"
+  desc "Housekeeping for docker environments"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.9.tar.gz"
-  sha256 "81a19c067c39d8ac4761a1fdf0d1d752ccc9afc083b2783ce74e5c5673fbc16f"
-  version "0.0.9"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.10.tar.gz"
+  sha256 "e9528e36fa87e7cd5650f1b46babef5ea6ea3e1fa857a20ab3f92cad33a7ebea"
+  version "0.0.10"
 
   depends_on "curl"
 
