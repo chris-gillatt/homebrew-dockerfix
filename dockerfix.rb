d413b8034de945e46be7c1296c0c7a60ea47fbf3
chris.gillatt <chris.gillatt@autotrader.co.uk>
2019-09-04 16:09:41 +0100

    -r attempt to start docker even when not running

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
   desc "Housekeeping for docker environments"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.12.tar.gz"
-  sha256 "6c426923bef257456d075dcf254199aa35beb7e4089738261fd1b841f25e6004"
-  version "0.0.12"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.13.tar.gz"
+  sha256 "ee7053e2a8f4102dbdd5b8b84132057a66d75bb86af3f199f10fc3c67de088fa"
+  version "0.0.13"
 
   depends_on "curl"
 
