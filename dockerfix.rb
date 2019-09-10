69a06b8df89b81267289b2575302408b0d3da1ef
chris.gillatt <chris.gillatt@autotrader.co.uk>
2019-09-10 10:42:11 +0100

    Add a simple progress bar for daemon restart

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
   desc "Housekeeping for docker environments"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.13.tar.gz"
-  sha256 "ee7053e2a8f4102dbdd5b8b84132057a66d75bb86af3f199f10fc3c67de088fa"
-  version "0.0.13"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.14.tar.gz"
+  sha256 "cc4450f319acf767b9416f3e77131ad17db0bd8a0fe4cfa13d4a2d82ebbd9455"
+  version "0.0.14"
 
   depends_on "curl"
 
