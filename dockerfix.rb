396249bb38a943c8e75f384f51c66bcf7096e64c
chris.gillatt <chris.gillatt@autotrader.co.uk>
2021-08-19 10:56:01 +0100

    Make stop all containers work faster

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
-  desc "Housekeeping convenience for docker environments"
+  desc "Convenient housekeeping for docker Mac users"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.16.tar.gz"
-  sha256 "dae562aef8e9fddc44c7c046e5fb112387709d47d6ef73974c9066e792207c3e"
-  version "0.0.16"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/raw/master/tars/dockerfix-0.0.17.tar.gz"
+  sha256 "2288c50949cc61347fb9af66f83f8c0b38d5442d231abf48ee92c80d1017cb3e"
+  version "0.0.17"
 
   depends_on "curl"
 
