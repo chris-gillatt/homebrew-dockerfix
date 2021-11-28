a22d6fe04d2805e918036ba32c4741a75db2182b
chris-gillatt <solex@hotmail.co.uk>
2021-11-28 16:07:49 +0000

    Automate releases; remove junk

---
+++ package.sh
@@ -0,0 +1,73 @@
+#!/bin/bash -xe
+# Brew package script
+export APP="dockerfix"
+REPO_NAME="homebrew-dockerfix"
+
+git config user.name "$USER"
+git config user.email "$USER_EMAIL"
+
+export RELEASE_COUNT=$(git rev-list --all --count)
+GIT_REVISION=$(git rev-parse HEAD)
+
+BRANCH="master"
+
+# Compress new version and place in tars archive
+mkdir tars
+tar cvf tars/"${APP}-0.0.${RELEASE_COUNT}.tar.gz" "$APP"
+
+# Generate Ruby file for Brew using template
+erb "${APP}.erb" > "${APP}.rb"
+
+## Git Tasks
+git add "${APP}.rb"
+# Commit files to repo
+git commit -m "Push $APP Release 0.0.${RELEASE_COUNT}" &&
+# Push to remote using shippr user
+git push https://"${USER}:${USER_PASSWORD}@github.com/${ORG}/${REPO_NAME}".git "$BRANCH"
+
+# Publish go cli binaries
+post_release_json()
+{
+  cat <<EOF
+{
+  "tag_name":         "0.0.${RELEASE_COUNT}",
+  "target_commitish": "${GIT_REVISION}",
+  "name":             "0.0.${RELEASE_COUNT}"
+}
+EOF
+}
+
+echo "Creating release.."
+
+NEW_RELEASE_RESPONSE=$(curl --silent \
+                            --write-out "\n%{http_code}" \
+                            -u "$USER:$USER_PASSWORD" \
+                            -H "Accept: application/json" \
+                            -H "Content-Type:application/json" \
+                            -X POST "https://api.github.com/repos/${ORG}/${REPO_NAME}/releases" \
+                            --data "$(post_release_json)")
+STATUS_CODE=$(echo "$NEW_RELEASE_RESPONSE" | tail -n 1)
+NEW_RELEASE=$(echo "$NEW_RELEASE_RESPONSE" | sed '$d')
+
+if [[ $STATUS_CODE -ge 400 ]]; then
+  echo 'ERROR: Failed to create release'
+  echo "$STATUS_CODE"
+  echo "$NEW_RELEASE"
+  exit 1
+fi
+
+echo "Release created"
+
+UPLOAD_URL=$(echo "$NEW_RELEASE" | jq -r .upload_url | cut -f1 -d"{")
+
+echo "Uploading binaries"
+
+curl --fail \
+     -u "${USER}:${USER_PASSWORD}" \
+     -H "Content-Type:application/octet-stream" \
+     -X POST "${UPLOAD_URL}?name=${APP}-0.0.${RELEASE_COUNT}.tar.gz" \
+     --data-binary "@tars/${APP}-0.0.${RELEASE_COUNT}.tar.gz" \
+     | jq -rc '.name + " - " + .url + " - " + .state'
+
+echo "$0 Done."
+
