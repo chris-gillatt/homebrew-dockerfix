cdf3af2a77b3036836803f221925d592898a67ef
chris-gillatt <solex@hotmail.co.uk>
2022-08-21 21:27:25 +0000

    Push dockerfix Release 0.1.6

--- dockerfix.rb
+++ dockerfix.rb
@@ -1,9 +1,9 @@
 class Dockerfix < Formula
   desc "Convenient housekeeping for docker Mac users"
   homepage "https://github.com/chris-gillatt/homebrew-dockerfix"
-  url "https://github.com/chris-gillatt/homebrew-dockerfix/releases/download/0.1.5/dockerfix-0.1.5.tar.gz"
-  sha256 "014cdc1e8cb29db229f775f662bff891fec8c371dbf78b9d1631d0834853fa70"
-  version "0.1.5"
+  url "https://github.com/chris-gillatt/homebrew-dockerfix/releases/download/0.1.6/dockerfix-0.1.6.tar.gz"
+  sha256 "5080a1b860831da237fd4ab9e62367f82e2722394489f14c3486e78fdf7849ca"
+  version "0.1.6"
 
   depends_on "curl"
 
